5b003562ba391173a76a55adabf4f82faf987110
ajaynayak <ajaynayak@gmail.com>
2025-02-13 11:49:07 -0800

    Adding a new BaseClientModel and datasets methods

---
+++ python/galileo/base.py
@@ -0,0 +1,39 @@
+from os import getenv
+
+from typing import Optional
+
+from galileo.client import AuthenticatedClient
+
+
+class BaseClientModel:
+    client: Optional[AuthenticatedClient] = None
+
+    @staticmethod
+    def _log_request(request):
+        print(
+            f"Request event hook: {request.method} {request.url} - Waiting for response"
+        )
+
+    @staticmethod
+    def _log_response(response):
+        request = response.request
+        print(
+            f"Response event hook: {request.method} {request.url} - Status {response.status_code}"
+        )
+
+    @staticmethod
+    def _create_client() -> AuthenticatedClient:
+        base_url = getenv("GALILEO_CONSOLE_URL")
+        api_key = getenv("GALILEO_API_KEY")
+        return AuthenticatedClient(
+            base_url=base_url,
+            api_key=api_key,
+            # httpx_args={
+            #     "event_hooks": {"request": [_log_request], "response": [_log_response]}
+            # },
+        )
+
+    def _get_client(self) -> AuthenticatedClient:
+        if self.client is None:
+            self.client = self._create_client()
+        return self.client
---
+++ python/galileo/datasets.py
@@ -0,0 +1,68 @@
+from typing import Union
+
+from galileo.models.dataset_db import DatasetDB
+from galileo.models.list_dataset_params import ListDatasetParams
+from galileo.models.list_dataset_response import ListDatasetResponse
+from galileo.models.dataset_name_filter import DatasetNameFilter
+from galileo.models.dataset_name_filter_operator import DatasetNameFilterOperator
+from galileo.api.datasets import (
+    list_datasets_datasets_get,
+    query_datasets_datasets_query_post,
+    update_dataset_content_datasets_dataset_id_content_patch,
+    update_dataset_datasets_dataset_id_patch,
+)
+from galileo.models.dataset_updated_at_sort import DatasetUpdatedAtSort
+from galileo.types import Response, Unset
+
+from galileo.base import BaseClientModel
+
+
+class Dataset:
+    def __init__(self, dataset_db: DatasetDB) -> None:
+        self._dataset = dataset_db
+
+    def add_row(self, row_data: dict) -> "Dataset":
+        # TODO: implement
+        return Dataset()
+
+    def __getattr__(self, attr):
+        """
+        Delegate attribute access to the underlying DatasetDB instance.
+        This allows you to use all the properties and methods of DatasetDB.
+        """
+        return getattr(self._dataset, attr)
+
+
+class Datasets(BaseClientModel):
+    def list(self, limit: Union[Unset, int] = 100) -> list[Dataset]:
+        datasets: ListDatasetResponse = list_datasets_datasets_get.sync(
+            client=self._get_client(), limit=limit
+        )
+        return [Dataset(dataset) for dataset in datasets.datasets] if datasets else []
+
+    def get(self, name: str) -> Dataset | None:
+        filter = DatasetNameFilter(operator=DatasetNameFilterOperator.EQ, value=name)
+        params = ListDatasetParams(
+            filters=[filter], sort=DatasetUpdatedAtSort(ascending=False)
+        )
+        datasets: ListDatasetResponse = query_datasets_datasets_query_post.sync(
+            client=self._get_client(), body=params, limit=1
+        )
+
+        if datasets and len(datasets.datasets) > 0:
+            return Dataset(datasets.datasets[0])
+        else:
+            return None
+
+
+#
+# Convenience functions
+#
+
+
+def get_dataset(name: str) -> Dataset | None:
+    return Datasets().get(name=name)
+
+
+def list_datasets(limit: Union[Unset, int] = 100) -> list[Dataset]:
+    return Datasets().list(limit=limit)
