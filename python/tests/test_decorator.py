5ecc540911f3b5abb035d588b6450d2924b44424
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2025-02-18 18:08:47 +0100

    tests: fix existing tests

--- python/tests/test_decorator.py
+++ python/tests/test_decorator.py
@@ -1,27 +1,18 @@
-from json import dumps
 from unittest.mock import AsyncMock, Mock, patch
 
-import pytest
-
-from pydantic import UUID4
+from uuid import UUID
 
 from galileo import log, galileo_context
 from galileo.schema.trace import TracesIngestRequest, TracesIngestResponse
 from galileo_core.schemas.shared.traces.trace import Trace, LlmSpan, WorkflowSpan
-
-from galileo_core.schemas.shared.workflows.node_type import NodeType
 
 
 def setup_mock_api_client(mock_api_client: Mock):
     mock_instance = mock_api_client.return_value
     mock_ingest_traces_sync = AsyncMock(return_value={})
     mock_ingest_traces_async = AsyncMock(return_value={})
-    mock_instance.get_project_by_name = AsyncMock(
-        return_value={"id": UUID4("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9a")}
-    )
-    mock_instance.get_log_stream_by_name = AsyncMock(
-        return_value={"id": UUID4("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9b")}
-    )
+    mock_instance.get_project_by_name = AsyncMock(return_value={"id": UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9a")})
+    mock_instance.get_log_stream_by_name = AsyncMock(return_value={"id": UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9b")})
     mock_instance.ingest_traces_sync = mock_ingest_traces_sync
     mock_instance.ingest_traces = mock_ingest_traces_async
     return mock_instance
@@ -43,14 +34,8 @@ def test_decorator_llm_span(mock_api_client: Mock) -> None:
     assert len(payload.traces) == 1
     assert len(payload.traces[0].spans) == 1
     assert type(payload.traces[0].spans[0]) == LlmSpan
-    assert payload.traces[0].input == {
-        "args": [],
-        "kwargs": {"query": "input"},
-    }
-    assert payload.traces[0].spans[0].input == {
-        "args": [],
-        "kwargs": {"query": "input"},
-    }
+    assert payload.traces[0].input == {"args": [], "kwargs": {"query": "input"}}
+    assert payload.traces[0].spans[0].input == {"args": [], "kwargs": {"query": "input"}}
     assert payload.traces[0].spans[0].output == output
 
 
@@ -76,14 +61,8 @@ def test_decorator_nested_span(mock_api_client: Mock) -> None:
     assert len(payload.traces[0].spans[0].spans) == 1
     assert type(payload.traces[0].spans[0]) == WorkflowSpan
     assert type(payload.traces[0].spans[0].spans[0]) == LlmSpan
-    assert payload.traces[0].input == {
-        "args": [],
-        "kwargs": {"nested_query": "input"},
-    }
-    assert payload.traces[0].spans[0].spans[0].input == {
-        "args": [],
-        "kwargs": {"query": "input"},
-    }
+    assert payload.traces[0].input == {"args": [], "kwargs": {"nested_query": "input"}}
+    assert payload.traces[0].spans[0].spans[0].input == {"args": [], "kwargs": {"query": "input"}}
     assert payload.traces[0].spans[0].output == output
     assert payload.traces[0].spans[0].spans[0].output == output
 
@@ -113,13 +92,7 @@ def test_decorator_multiple_nested_spans(mock_api_client: Mock) -> None:
     assert type(payload.traces[0].spans[0]) == WorkflowSpan
     assert type(payload.traces[0].spans[0].spans[0]) == LlmSpan
     assert type(payload.traces[0].spans[0].spans[1]) == LlmSpan
-    assert payload.traces[0].input == {
-        "args": [],
-        "kwargs": {"nested_query": "input"},
-    }
-    assert payload.traces[0].spans[0].spans[0].input == {
-        "args": [],
-        "kwargs": {"query": "input"},
-    }
+    assert payload.traces[0].input == {"args": [], "kwargs": {"nested_query": "input"}}
+    assert payload.traces[0].spans[0].spans[0].input == {"args": [], "kwargs": {"query": "input"}}
     assert payload.traces[0].spans[0].output == output
     assert payload.traces[0].spans[0].spans[0].output == "response"
