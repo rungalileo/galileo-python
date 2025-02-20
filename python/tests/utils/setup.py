f83b1284af50f90dad0a51f789228a3717707dc1
ajaynayak <ajaynayak@gmail.com>
2025-02-20 08:48:19 -0800

    Update python/tests/utils/setup.py
    Co-authored-by: Andrii Soldatenko <andrii.soldatenko@gmail.com>

--- python/tests/utils/setup.py
+++ python/tests/utils/setup.py
@@ -48,7 +48,6 @@ def setup_mock_logstreams_client(mock_logstreams_client: Mock):
                 name="test",
                 created_at=now,
                 updated_at=now,
-                # additional_properties={},
             )
         )
     )
