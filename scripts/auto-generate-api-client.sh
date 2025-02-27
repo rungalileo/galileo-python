252c061d7798cef822b24dc6819f49e5864a7bf9
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2025-02-27 22:17:48 +0100

    fix: typo inside ./scripts/auto-generate-api-client.sh (#32)

--- scripts/auto-generate-api-client.sh
+++ scripts/auto-generate-api-client.sh
@@ -7,7 +7,7 @@
 # Define the paths to the OpenAPI spec and config file
 OPENAPI_SPEC_PATH="../openapi.yaml"
 CONFIG_PATH="../openapi-client-config.yaml"
-OUTPUT_PATH="../galileo/resources"
+OUTPUT_PATH="./src/galileo/resources"
 CLIENT_DIR_NAME="galileo_api_server_client"
 
 # Backup the current output path
