55b443243ca8a57e7289e7c747635f0bf72180fe
ajaynayak <ajaynayak@gmail.com>
2025-02-28 20:25:29 -0800

    modifying datetime serialization to work with python 3.12

--- src/galileo/utils/serialization.py
+++ src/galileo/utils/serialization.py
@@ -38,7 +38,9 @@ def serialize_datetime(v: dt.datetime) -> str:
     if v.tzinfo is not None:
         return _serialize_zoned_datetime(v)
     else:
-        local_tz = dt.datetime.now().astimezone().tzinfo
+        # First create a timezone-aware datetime using UTC, then convert to local
+        # This approach should work consistently across Python versions
+        local_tz = dt.datetime.now(dt.timezone.utc).astimezone().tzinfo
         localized_dt = v.replace(tzinfo=local_tz)
         return _serialize_zoned_datetime(localized_dt)
 
