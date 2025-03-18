cf3f6b94ee7b5812e880cca7aa75572ccf9a041c
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2025-03-18 22:14:09 +0100

    add basic test

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -1,13 +1,37 @@
 from datetime import datetime
-from unittest.mock import Mock, patch
+from unittest import mock
+from unittest.mock import Mock, patch, MagicMock
+from uuid import UUID
 
-from galileo.experiments import Experiments
+import pytest
+from pydantic import UUID4
+
+import galileo
+from galileo.projects import Project
+from galileo.resources.models import ExperimentResponse, ProjectType, ProjectCreateResponse
+from galileo.experiments import Experiments, run_experiment
+from tests.testutils.setup import setup_mock_core_api_client, setup_mock_projects_client, setup_mock_logstreams_client
+
+
+def project():
+    now = datetime.now()
+    return Project(
+        ProjectCreateResponse(created_at=now,
+        id=str(UUID(int=0)),
+        updated_at=now,
+        name = "awesome-new-project",
+        type_=ProjectType.GEN_AI,
+    ))
 
 
 class TestExperiments:
+    @pytest.fixture
+    @patch.object(galileo.experiments.Projects, "get")
+    def mock_dependency(self, mock_projects_get: Mock, project: Project):
+        mock_projects_get.return_value = project
+
     @patch("galileo.experiments.create_experiment_v2_projects_project_id_experiments_post")
     def test_create(self, galileo_resources_api_create_experiment: Mock):
-        from galileo.resources.models import ExperimentResponse
 
         now = datetime(2020, 1, 1).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
         galileo_resources_api_create_experiment.sync = Mock(
@@ -18,3 +42,20 @@ class TestExperiments:
         experiment = Experiments().create(project_id="test", name="test_experiment")
         assert experiment.name == "test_experiment"
         assert experiment.project_id == "test"
+
+
+    @patch.object(galileo.experiments.Projects, "get", return_value=project())
+    @patch.object(galileo.experiments.Experiments, "get", return_value=experiment())
+    def test_run_experiment_with_function(self, mock_get_project: Mock):
+        dataset = [
+            {"name": "Lola"},
+            {"name": "Jo"},
+        ]
+        experiment = run_experiment(
+            "test_experiment",
+            project="awesome-new-project",
+            dataset=dataset,
+            function=lambda *args, **kwargs: "content",
+            metrics=["output_tone"]
+        )
+        assert experiment.name == "test_experiment"
