48aa041b5a4fcfaae445cce4fad55c94e9c86204
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2025-03-18 16:12:20 +0100

    feat: add prompt_settings to run_experiment

--- src/galileo/jobs.py
+++ src/galileo/jobs.py
@@ -2,7 +2,7 @@ import logging
 
 from galileo.base import BaseClientModel
 from galileo.resources.api.jobs import create_job_jobs_post
-from galileo.resources.models import CreateJobRequest, ScorerConfig, TaskType
+from galileo.resources.models import CreateJobRequest, ScorerConfig, TaskType, PromptRunSettings
 
 _logger = logging.getLogger(__name__)
 
@@ -17,13 +17,14 @@ class Jobs(BaseClientModel):
         prompt_template_id: str,
         task_type: TaskType,
         scorers: list[ScorerConfig],
+        prompt_settings: PromptRunSettings,
     ):
         create_params = dict(
             project_id=project_id,
             dataset_id=dataset_id,
             job_name=name,
             run_id=run_id,
-            prompt_settings={},
+            prompt_settings=prompt_settings,
             prompt_template_version_id=prompt_template_id,
             task_type=task_type,
             scorers=scorers,
