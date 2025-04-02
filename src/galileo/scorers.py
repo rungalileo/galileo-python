8798d945a2925831a4124adb5318c5bf8828d510
ajaynayak <ajaynayak@gmail.com>
2025-04-01 17:58:27 -0700

    enabling custom hosted scorers to be used

--- src/galileo/scorers.py
+++ src/galileo/scorers.py
@@ -6,6 +6,7 @@ from galileo.resources.api.run_scorer_settings import (
 from galileo.resources.models import (
     ListScorersRequest,
     RunScorerSettingsRequest,
+    ScorerConfig,
     ScorerTypeFilter,
     ScorerTypeFilterOperator,
     ScorerTypes,
@@ -13,16 +14,30 @@ from galileo.resources.models import (
 
 
 class Scorers(BaseClientModel):
-    def list(self):
+    def list(self, types: list[ScorerTypes] = None):
+        """
+        Args:
+            types: List of scorer types to filter by. Defaults to all scorers.
+        Returns:
+            List of scorers
+        """
         body = ListScorersRequest(
-            filters=[ScorerTypeFilter(value=ScorerTypes.PRESET, operator=ScorerTypeFilterOperator.EQ)]
+            filters=[ScorerTypeFilter(value=type, operator=ScorerTypeFilterOperator.EQ) for type in types]
         )
         result = list_scorers_with_filters_scorers_list_post.sync(client=self.client, body=body)
         return result.scorers
 
 
 class ScorerSettings(BaseClientModel):
-    def create(self, project_id, run_id, scorers):
+    def create(self, project_id: str, run_id: str, scorers: list[ScorerConfig]):
+        """
+        Args:
+            project_id: ID of the project
+            run_id: ID of the run
+            scorers: List of scorer configurations
+        Returns:
+            Upserted scorer settings
+        """
         return upsert_scorers_config_projects_project_id_runs_run_id_scorer_settings_post.sync(
             project_id=project_id,
             run_id=run_id,
