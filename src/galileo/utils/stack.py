bbff3d4e2f833acd7c739296bac73d462549804a
Robert Friel <rob@galileo.ai>
2025-05-14 14:17:42 -0700

    oops

---
+++ src/galileo/utils/stack.py
@@ -0,0 +1,31 @@
+import inspect
+import logging
+from typing import Optional
+
+from galileo_core.schemas.logging.code import LoggedStack, LoggedStackFrame
+
+from sentry_sdk.utils import serialize_frame
+
+_logger = logging.getLogger(__name__)
+
+
+def get_stack_trace() -> Optional[LoggedStack]:
+    """
+    Get the stack trace for the current function.
+    """
+    _logger.info("Getting stack trace")
+    frames = []
+    for frame_info in inspect.stack():
+        if "galileo-python/src/galileo/" in frame_info.filename:
+            continue
+        try:
+            serialized = serialize_frame(frame_info.frame)
+            _logger.info(f"Stack frame: {serialized}")
+            frames.append(LoggedStackFrame.model_validate(serialized))
+            _logger.info("Stack frame added")
+        except Exception as e:
+            _logger.error(f"Error while serializing stack frame: {e}", exc_info=True)
+            continue
+    return LoggedStack(frames=frames)
+
+
