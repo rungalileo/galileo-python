521f11a3d4cfa43ab73d3203d4183372f4f56263
aseembits93 <aseem.bits@gmail.com>
2025-05-28 12:03:41 -0700

    new pr

---
+++ src/galileo/sorter.py
@@ -0,0 +1,10 @@
+def sorter(arr: list[float]) -> list[float]:
+    print("codeflash stdout: Sorting list")
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    print(f"result: {arr}")
+    return arr
