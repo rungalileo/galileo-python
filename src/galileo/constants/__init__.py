6a1e64e5698eb76d6f9276aa44007d0c26ec265a
franz101 <fkrekeler@gmail.com>
2025-06-24 11:43:56 +0100

    Update src/galileo/constants/__init__.py
    Co-authored-by: Andrii Soldatenko <andrii.soldatenko@gmail.com>

--- src/galileo/constants/__init__.py
+++ src/galileo/constants/__init__.py
@@ -2,6 +2,6 @@ DEFAULT_PROJECT_NAME = "default"
 DEFAULT_LOG_STREAM_NAME = "default"
 
 DEFAULT_API_URL = "https://api.galileo.ai/"
-FALLBACK_API_URL = "https://app.galileo.ai/"
+DEFAULT_APP_URL = "https://app.galileo.ai/"
 
 __all__ = ("DEFAULT_PROJECT_NAME", "DEFAULT_LOG_STREAM_NAME", "DEFAULT_API_URL", "FALLBACK_API_URL")
