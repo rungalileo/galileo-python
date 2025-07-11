499b59b0f0c34e9445d9b2eb4d0860b0b532a60f
ShriyaEduk <shriya@galileo.ai>
2025-07-11 13:14:45 -0700

    apply the exisitng patch http validation error script

--- src/galileo/resources/models/http_validation_error.py
+++ src/galileo/resources/models/http_validation_error.py
@@ -8,8 +8,6 @@ from ..types import UNSET, Unset
 
 if TYPE_CHECKING:
     from ..models.validation_error import ValidationError
-
-
 T = TypeVar("T", bound="HTTPValidationError")
 
 
@@ -30,13 +28,11 @@ class HTTPValidationError:
             for detail_item_data in self.detail:
                 detail_item = detail_item_data.to_dict()
                 detail.append(detail_item)
-
         field_dict: dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update({})
         if detail is not UNSET:
             field_dict["detail"] = detail
-
         return field_dict
 
     @classmethod
@@ -46,13 +42,13 @@ class HTTPValidationError:
         d = dict(src_dict)
         detail = []
         _detail = d.pop("detail", UNSET)
-        for detail_item_data in _detail or []:
-            detail_item = ValidationError.from_dict(detail_item_data)
-
-            detail.append(detail_item)
-
+        if isinstance(_detail, str):
+            detail.append(ValidationError(loc=["api"], msg=_detail, type_="string"))
+        else:
+            for item in _detail or []:
+                detail_item = ValidationError.from_dict(item)
+                detail.append(detail_item)
         http_validation_error = cls(detail=detail)
-
         http_validation_error.additional_properties = d
         return http_validation_error
 
