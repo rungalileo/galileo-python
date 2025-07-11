9c699f4b6939ab5f4a069d9677197f60bb9228f7
galileo-automation <ci@rungalileo.io>
2025-07-11 22:26:20 +0000

    chore(release): v1.8.1
    Automatically generated by python-semantic-release

--- src/galileo/__init__.py
+++ src/galileo/__init__.py
@@ -16,4 +16,4 @@ from galileo_core.schemas.logging.trace import Trace
 from galileo_core.schemas.protect.execution_status import ExecutionStatus
 from galileo_core.schemas.protect.stage import StageType
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
