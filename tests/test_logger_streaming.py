c8ef8fb6748cb1d37b879be170a3ada3b717f82e
ajaynayak <ajaynayak@gmail.com>
2025-07-17 09:20:28 -0700

    adding tests

--- tests/test_logger_streaming.py
+++ tests/test_logger_streaming.py
@@ -2,10 +2,12 @@ import asyncio
 import datetime
 import logging
 from unittest.mock import Mock, patch
+from uuid import UUID
 
 import pytest
 
 from galileo.logger import GalileoLogger
+from galileo.logger.logger import GalileoLoggerException
 from galileo.schema.trace import SpansIngestRequest, SpanUpdateRequest, TracesIngestRequest, TraceUpdateRequest
 from galileo_core.schemas.logging.llm import Message
 from galileo_core.schemas.shared.document import Document
@@ -1182,3 +1184,435 @@ def test_trace_with_nested_span_and_sibling(
     assert request.output == "response2"
     assert request.status_code == 200
     assert request.is_complete
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_add_llm_span_and_conclude_existing_trace(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock
+) -> None:
+    mock_core_api_client_instance = setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    created_at = datetime.datetime.now()
+    metadata = {"key": "value"}
+    logger = GalileoLogger(
+        project="my_project",
+        log_stream="my_log_stream",
+        mode="streaming",
+        trace_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d",
+    )
+
+    mock_core_api_client_instance.get_trace_sync.assert_called_once()
+
+    assert len(logger.traces) == 1
+    assert len(logger._parent_stack) == 1
+    assert logger._parent_stack[0].id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert logger._parent_stack[0].name == "test-trace"
+    assert logger._parent_stack[0].type == "trace"
+    assert len(logger._parent_stack[0].spans) == 0
+
+    capture = setup_thread_pool_request_capture(logger)
+
+    logger.add_llm_span(
+        input="prompt",
+        output="response",
+        model="gpt4o",
+        name="test-span",
+        tools=[{"name": "tool1", "args": {"arg1": "val1"}}],
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.conclude(output="response", status_code=200, duration_ns=1_000_000)
+
+    captured_task = capture.get_task_by_function_name("ingest_spans_with_backoff")
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.spans[0].input == [Message(role="user", content="prompt")]
+    assert request.spans[0].output == Message(role="assistant", content="response")
+    assert request.spans[0].name == "test-span"
+    assert request.spans[0].model == "gpt4o"
+    assert request.spans[0].tools == [{"name": "tool1", "args": {"arg1": "val1"}}]
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+
+    captured_task = capture.get_task_by_function_name("update_trace_with_backoff")
+    request = captured_task.request
+    assert isinstance(request, TraceUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_trace.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.output == "response"
+    assert request.status_code == 200
+    assert request.is_complete
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_add_nested_span_and_conclude_existing_trace(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock
+) -> None:
+    mock_core_api_client_instance = setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    created_at = datetime.datetime.now()
+    metadata = {"key": "value"}
+    logger = GalileoLogger(
+        project="my_project",
+        log_stream="my_log_stream",
+        mode="streaming",
+        trace_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d",
+    )
+
+    mock_core_api_client_instance.get_trace_sync.assert_called_once()
+
+    assert len(logger.traces) == 1
+    assert len(logger._parent_stack) == 1
+    assert logger._parent_stack[0].id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert logger._parent_stack[0].name == "test-trace"
+    assert logger._parent_stack[0].type == "trace"
+    assert len(logger._parent_stack[0].spans) == 0
+
+    capture = setup_thread_pool_request_capture(logger)
+
+    logger.add_workflow_span(
+        input="workflow-input",
+        output="workflow-output",
+        name="test-workflow-span",
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.add_llm_span(
+        input="prompt",
+        output="response",
+        model="gpt4o",
+        name="test-span",
+        tools=[{"name": "tool1", "args": {"arg1": "val1"}}],
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.conclude(output="workflow-output", status_code=200, duration_ns=1_000_000)
+
+    logger.conclude(output="response", status_code=200, duration_ns=1_000_000)
+
+    captured_tasks = capture.get_all_tasks()
+    assert len(captured_tasks) == 4
+
+    captured_task = captured_tasks[0]
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.spans[0].type == "workflow"
+    assert request.spans[0].input == "workflow-input"
+    assert request.spans[0].output == "workflow-output"
+    assert request.spans[0].name == "test-workflow-span"
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+    workflow_span_id = request.spans[0].id
+
+    captured_task = captured_tasks[1]
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == workflow_span_id
+    assert request.spans[0].type == "llm"
+    assert request.spans[0].input == [Message(role="user", content="prompt")]
+    assert request.spans[0].output == Message(role="assistant", content="response")
+    assert request.spans[0].name == "test-span"
+    assert request.spans[0].model == "gpt4o"
+    assert request.spans[0].tools == [{"name": "tool1", "args": {"arg1": "val1"}}]
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+
+    captured_task = captured_tasks[2]
+    request = captured_task.request
+    assert isinstance(request, SpanUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_span.assert_called_with(request)
+
+    assert request.span_id == workflow_span_id
+    assert request.output == "workflow-output"
+    assert request.status_code == 200
+
+    captured_task = captured_tasks[3]
+    request = captured_task.request
+    assert isinstance(request, TraceUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_trace.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.output == "response"
+    assert request.status_code == 200
+    assert request.is_complete
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_add_llm_span_and_conclude_existing_workflow_span(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock
+) -> None:
+    mock_core_api_client_instance = setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    created_at = datetime.datetime.now()
+    metadata = {"key": "value"}
+    logger = GalileoLogger(
+        project="my_project",
+        log_stream="my_log_stream",
+        mode="streaming",
+        span_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e",
+    )
+
+    mock_core_api_client_instance.get_span_sync.assert_called_once()
+
+    assert len(logger.traces) == 1
+    assert len(logger._parent_stack) == 1
+    assert logger._parent_stack[0].id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert logger._parent_stack[0].name == "test-workflow-span"
+    assert logger._parent_stack[0].type == "workflow"
+    assert len(logger._parent_stack[0].spans) == 0
+
+    capture = setup_thread_pool_request_capture(logger)
+
+    logger.add_llm_span(
+        input="prompt",
+        output="response",
+        model="gpt4o",
+        name="test-span",
+        tools=[{"name": "tool1", "args": {"arg1": "val1"}}],
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.conclude(output="response", status_code=200, duration_ns=1_000_000)
+
+    captured_task = capture.get_task_by_function_name("ingest_spans_with_backoff")
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert request.spans[0].input == [Message(role="user", content="prompt")]
+    assert request.spans[0].output == Message(role="assistant", content="response")
+    assert request.spans[0].name == "test-span"
+    assert request.spans[0].model == "gpt4o"
+    assert request.spans[0].tools == [{"name": "tool1", "args": {"arg1": "val1"}}]
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+
+    captured_task = capture.get_task_by_function_name("update_span_with_backoff")
+    request = captured_task.request
+    assert isinstance(request, SpanUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_span.assert_called_with(request)
+
+    assert request.span_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert request.output == "response"
+    assert request.status_code == 200
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_add_nested_span_and_conclude_existing_span(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock
+) -> None:
+    mock_core_api_client_instance = setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    created_at = datetime.datetime.now()
+    metadata = {"key": "value"}
+    logger = GalileoLogger(
+        project="my_project",
+        log_stream="my_log_stream",
+        mode="streaming",
+        span_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e",
+    )
+
+    mock_core_api_client_instance.get_span_sync.assert_called_once()
+
+    assert len(logger.traces) == 1
+    assert len(logger._parent_stack) == 1
+    assert logger._parent_stack[0].id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert logger._parent_stack[0].name == "test-workflow-span"
+    assert logger._parent_stack[0].type == "workflow"
+    assert len(logger._parent_stack[0].spans) == 0
+
+    capture = setup_thread_pool_request_capture(logger)
+
+    logger.add_workflow_span(
+        input="workflow-input",
+        output="workflow-output",
+        name="test-workflow-span-2",
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.add_llm_span(
+        input="prompt",
+        output="response",
+        model="gpt4o",
+        name="test-span",
+        tools=[{"name": "tool1", "args": {"arg1": "val1"}}],
+        duration_ns=1_000_000,
+        created_at=created_at,
+        metadata=metadata,
+        step_number=1,
+    )
+
+    logger.conclude(output="workflow-output-2", status_code=200, duration_ns=1_000_000)
+
+    logger.conclude(output="response", status_code=200, duration_ns=1_000_000)
+
+    captured_tasks = capture.get_all_tasks()
+    assert len(captured_tasks) == 4
+
+    captured_task = captured_tasks[0]
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert request.spans[0].type == "workflow"
+    assert request.spans[0].input == "workflow-input"
+    assert request.spans[0].output == "workflow-output"
+    assert request.spans[0].name == "test-workflow-span-2"
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+    workflow_span_id = request.spans[0].id
+
+    captured_task = captured_tasks[1]
+    request = captured_task.request
+    assert isinstance(request, SpansIngestRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.ingest_spans.assert_called_with(request)
+
+    assert request.trace_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+    assert request.parent_id == workflow_span_id
+    assert request.spans[0].type == "llm"
+    assert request.spans[0].input == [Message(role="user", content="prompt")]
+    assert request.spans[0].output == Message(role="assistant", content="response")
+    assert request.spans[0].name == "test-span"
+    assert request.spans[0].model == "gpt4o"
+    assert request.spans[0].tools == [{"name": "tool1", "args": {"arg1": "val1"}}]
+    assert request.spans[0].created_at == created_at
+    assert request.spans[0].user_metadata == metadata
+    assert request.spans[0].metrics.duration_ns == 1_000_000
+    assert request.spans[0].step_number == 1
+
+    captured_task = captured_tasks[2]
+    request = captured_task.request
+    assert isinstance(request, SpanUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_span.assert_called_with(request)
+
+    assert request.output == "workflow-output-2"
+    assert request.status_code == 200
+
+    captured_task = captured_tasks[3]
+    request = captured_task.request
+    assert isinstance(request, SpanUpdateRequest)
+
+    asyncio.run(captured_task.task_func())
+    mock_core_api_client_instance.update_span.assert_called_with(request)
+
+    assert request.span_id == UUID("6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+    assert request.output == "response"
+    assert request.status_code == 200
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_catch_error_trace_span_ids_in_batch_mode(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock, caplog
+) -> None:
+    setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    with pytest.raises(GalileoLoggerException):
+        GalileoLogger(project="my_project", log_stream="my_log_stream", trace_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9d")
+
+    with pytest.raises(GalileoLoggerException):
+        GalileoLogger(project="my_project", log_stream="my_log_stream", span_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e")
+
+
+@patch("galileo.logger.logger.LogStreams")
+@patch("galileo.logger.logger.Projects")
+@patch("galileo.logger.logger.GalileoCoreApiClient")
+def test_catch_error_mismatched_trace_span_ids(
+    mock_core_api_client: Mock, mock_projects_client: Mock, mock_logstreams_client: Mock, caplog
+) -> None:
+    setup_mock_core_api_client(mock_core_api_client)
+    setup_mock_projects_client(mock_projects_client)
+    setup_mock_logstreams_client(mock_logstreams_client)
+
+    with caplog.at_level(logging.WARNING):
+        GalileoLogger(
+            project="my_project",
+            log_stream="my_log_stream",
+            mode="streaming",
+            trace_id="00000000-0000-0000-0000-000000000000",
+            span_id="6c4e3f7e-4a9a-4e7e-8c1f-3a9a3a9a3a9e",
+        )
+
+    assert "does not belong to trace" in caplog.text
