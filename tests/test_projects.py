85062fcbc88b96b52bbb47f119ae0c145f7be118
Jonathan Rhone <jonathanrhone@rungalileo.io>
2025-07-25 15:40:40 -0400

    fix project test

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,17 +1,14 @@
 import logging
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from galileo.projects import Projects
 
 
 @patch("galileo.projects.get_all_projects_projects_all_get")
 def test_get_all_projects_projects_all_get_exc(get_all_projects_projects_all_get, caplog):
-    get_all_projects_projects_all_get.sync = Mock(side_effect=ValueError("unable to get all projects"))
+    get_all_projects_projects_all_get.sync.side_effect = ValueError("unable to get all projects")
 
-    mock_config = Mock()
-    mock_client = Mock()
-    mock_config.api_client = mock_client
-    projects_client = Projects(config=mock_config)
+    projects_client = Projects()
     # it doesn't trough exception, because we catch all and log
     projects_client.list()
 
