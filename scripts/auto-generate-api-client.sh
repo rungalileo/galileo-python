c49dbd741805947fcc664f4565c5fd81d7a32550
ajaynayak <ajaynayak@gmail.com>
2025-07-29 13:50:22 -0700

    post-merge fixes

--- scripts/auto-generate-api-client.sh
+++ scripts/auto-generate-api-client.sh
@@ -8,7 +8,6 @@
 OPENAPI_SPEC_PATH="../openapi.yaml"
 CONFIG_PATH="../openapi-client-config.yaml"
 OUTPUT_PATH="../src/galileo/resources"
-CLIENT_DIR_NAME="fast_api_client"
 
 HOME_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -21,13 +20,6 @@ mv "$OUTPUT_PATH"  "$OUTPUT_PATH"_backup
 
 # Run the OpenAPI Python client generator
 openapi-python-client generate --meta none  --path "$OPENAPI_SPEC_PATH" --output-path "$OUTPUT_PATH" --custom-template-path ../codegen_templates --config "$CONFIG_PATH"
-
-# Remove unnecessary files
-rm "$OUTPUT_PATH"/pyproject.toml
-
-# Flatten the structure by moving the client directory to the output path
-cp -r "$OUTPUT_PATH"/"$CLIENT_DIR_NAME"/* "$OUTPUT_PATH"
-rm -r "$OUTPUT_PATH"/"$CLIENT_DIR_NAME"
 
 # Remove the backup directory
 rm -r "$OUTPUT_PATH"_backup
