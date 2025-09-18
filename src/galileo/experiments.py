919e9291eca230e2d484714262d145701c73986b
Nachiket Paranjape <nachiket@galileo.ai>
2025-09-18 05:50:14 -0700

    feat: Enable metrics for LogStream (#321)

--- src/galileo/experiments.py
+++ src/galileo/experiments.py
@@ -19,8 +19,8 @@ from galileo.resources.api.experiment import (
 from galileo.resources.models import ExperimentResponse, HTTPValidationError, PromptRunSettings, ScorerConfig, TaskType
 from galileo.schema.datasets import DatasetRecord
 from galileo.schema.metrics import GalileoScorers, LocalMetricConfig, Metric
-from galileo.scorers import Scorers, ScorerSettings
 from galileo.utils.datasets import load_dataset_and_records
+from galileo.utils.metrics import create_metric_configs
 
 _logger = logging.getLogger(__name__)
 
@@ -88,53 +88,6 @@ class Experiments(BaseClientModel):
 
     def list(self, project_id: str) -> Optional[Union[HTTPValidationError, list["ExperimentResponse"]]]:
         return list_experiments_projects_project_id_experiments_get.sync(project_id=project_id, client=self.client)
-
-    @staticmethod
-    def create_metric_configs(
-        project_id: str,
-        experiment_id: str,
-        metrics: builtins.list[Union[GalileoScorers, Metric, LocalMetricConfig, str]],
-    ) -> tuple[builtins.list[ScorerConfig], builtins.list[LocalMetricConfig]]:
-        local_metric_configs: list[LocalMetricConfig] = []
-        scorer_name_versions: list[tuple[str, Optional[int]]] = []
-        for metric in metrics:
-            if isinstance(metric, GalileoScorers):
-                scorer_name_versions.append((metric.value, None))
-            elif isinstance(metric, Metric):
-                scorer_name_versions.append((metric.name, metric.version))
-            elif isinstance(metric, LocalMetricConfig):
-                local_metric_configs.append(metric)
-            elif isinstance(metric, str):
-                scorer_name_versions.append((metric, None))
-            else:
-                raise ValueError(f"Unknown metric type: {type(metric)}")
-
-        scorers: list[ScorerConfig] = []
-        if scorer_name_versions:
-            all_scorers = Scorers().list()
-            known_metrics = {metric.name: metric for metric in all_scorers}
-            unknown_metrics = []
-            for scorer_name, scorer_version in scorer_name_versions:
-                if scorer_name in known_metrics:
-                    raw_metric_dict = known_metrics[scorer_name].to_dict()
-
-                    # Set the version on the ScorerConfig if provided
-                    if scorer_version is not None:
-                        raw_version = Scorers().get_scorer_version(
-                            scorer_id=raw_metric_dict["id"], version=scorer_version
-                        )
-                        raw_metric_dict["scorer_version"] = raw_version.to_dict()
-                    scorers.append(ScorerConfig.from_dict(raw_metric_dict))
-                else:
-                    unknown_metrics.append(metric)
-            if unknown_metrics:
-                raise ValueError(
-                    "One or more non-existent metrics are specified:"
-                    + ", ".join(f"'{metric}'" for metric in unknown_metrics)
-                )
-            ScorerSettings().create(project_id=project_id, run_id=experiment_id, scorers=scorers)
-
-        return scorers, local_metric_configs
 
     def run(
         self,
@@ -311,7 +264,7 @@ def run_experiment(
     scorer_settings: Optional[list[ScorerConfig]] = None
     local_metrics: list[LocalMetricConfig] = list()
     if metrics is not None:
-        scorer_settings, local_metrics = Experiments.create_metric_configs(project_obj.id, experiment_obj.id, metrics)
+        scorer_settings, local_metrics = create_metric_configs(project_obj.id, experiment_obj.id, metrics)
 
     # Execute a runner function experiment
     if function is not None:
