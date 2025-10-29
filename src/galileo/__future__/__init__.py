1c1745887579564149341b76ce57be0de1d5de6d
Nachiket Paranjape <nachiket@galileo.ai>
2025-10-29 08:43:50 -0700

    merge

--- src/galileo/__future__/__init__.py
+++ src/galileo/__future__/__init__.py
@@ -17,16 +17,12 @@ from galileo.__future__.shared.exceptions import (
     ResourceNotFoundError,
     ValidationError,
 )
-<<<<<<< HEAD
-=======
+
 from galileo.__future__.log_stream import LogStream
 from galileo.__future__.metric import Metric
 from galileo.__future__.project import Project
 from galileo.__future__.prompt import Prompt
-from galileo.__future__.types import MetricSpec
->>>>>>> 33b9b0a (add/update)
 from galileo.schema.message import Message
-from galileo.schema.metrics import GalileoScorers, LocalMetricConfig
 from galileo.search import RecordType
 from galileo.utils.logging import enable_console_logging
 from galileo_core.schemas.logging.llm import MessageRole
@@ -38,13 +34,10 @@ __all__ = [
     "ConfigurationError",
     "Dataset",
     "GalileoFutureError",
-    "GalileoScorers",
-    "LocalMetricConfig",
     "LogStream",
     "Message",
     "MessageRole",
     "Metric",
-    "MetricSpec",
     "Project",
     "Prompt",
     "RecordType",
