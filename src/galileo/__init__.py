4759eedecb46fce786baf41436a257d10b08a257
galileo-automation <ci@rungalileo.io>
2025-11-07 18:09:27 +0000

    chore(release): v1.29.1
    Automatically generated by python-semantic-release

--- src/galileo/__init__.py
+++ src/galileo/__init__.py
@@ -29,4 +29,4 @@ from galileo_core.schemas.protect.response import Response
 from galileo_core.schemas.protect.ruleset import Ruleset
 from galileo_core.schemas.protect.stage import StageType
 
-__version__ = "1.29.0"
+__version__ = "1.29.1"
