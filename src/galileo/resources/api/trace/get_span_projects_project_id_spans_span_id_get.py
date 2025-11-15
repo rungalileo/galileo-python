266e6795005f1195206807517c1a6c73dfe38d69
Juan Andres Ramil <juan@rungalileo.io>
2025-11-14 22:49:49 -0300

    Generate resources

--- src/galileo/resources/api/trace/get_span_projects_project_id_spans_span_id_get.py
+++ src/galileo/resources/api/trace/get_span_projects_project_id_spans_span_id_get.py
@@ -3,7 +3,7 @@ from typing import Any, Optional, Union
 
 import httpx
 
-from galileo.utils.headers_data import get_package_version
+from galileo.utils.headers_data import get_sdk_header
 from galileo_core.constants.request_method import RequestMethod
 from galileo_core.helpers.api_client import ApiClient
 
@@ -26,7 +26,7 @@ def _get_kwargs(project_id: str, span_id: str) -> dict[str, Any]:
         "path": f"/projects/{project_id}/spans/{span_id}",
     }
 
-    headers["X-Galileo-SDK"] = f"galileo-python/{get_package_version()}"
+    headers["X-Galileo-SDK"] = get_sdk_header()
 
     _kwargs["content_headers"] = headers
     return _kwargs
