30f72b319eeb1ba853f5e928fd3d09cfa86f37a7
Pratyusha Duvvuri <pratyusha@galileo.ai>
2025-11-19 18:13:08 +0300

    feat: Adding decorator support (#410)

--- src/galileo/__init__.py
+++ src/galileo/__init__.py
@@ -4,6 +4,7 @@
 # ruff: noqa: F401
 
 from galileo.decorator import GalileoDecorator, galileo_context, log
+from galileo.exceptions import GalileoLoggerException
 from galileo.logger import GalileoLogger
 from galileo.protect import ainvoke_protect, invoke_protect
 from galileo.schema.message import Message
@@ -15,6 +16,7 @@ from galileo.stages import (
     resume_protect_stage,
     update_protect_stage,
 )
+from galileo.tracing import get_tracing_headers
 from galileo.utils.logging import enable_console_logging
 from galileo_core.helpers.api_key import create_api_key, delete_api_key, list_api_keys
 from galileo_core.helpers.dependencies import is_dependency_available
