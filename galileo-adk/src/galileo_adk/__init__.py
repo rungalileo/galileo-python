50a5891fdea7981281c537113f645be99aec0e75
Sankar Avula <sankar@galileo.ai>
2026-02-18 16:05:17 -0800

    feat: galileo adk to natively support custom retriever tools

--- galileo-adk/src/galileo_adk/__init__.py
+++ galileo-adk/src/galileo_adk/__init__.py
@@ -1,11 +1,13 @@
 __version__ = "1.0.0-beta.1"
 
 from galileo_adk.callback import GalileoADKCallback
+from galileo_adk.decorator import galileo_retriever
 from galileo_adk.observer import get_custom_metadata
 from galileo_adk.plugin import GalileoADKPlugin
 
 __all__ = [
     "GalileoADKPlugin",
     "GalileoADKCallback",
+    "galileo_retriever",
     "get_custom_metadata",
 ]
