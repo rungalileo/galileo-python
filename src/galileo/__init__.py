bdcdede246a5c885e3afd8c8d48f02a3f3630ed0
galileo-automation <ci@rungalileo.io>
2026-02-25 17:35:15 +0000

    chore(release): v1.48.0
    Automatically generated by python-semantic-release

--- src/galileo/__init__.py
+++ src/galileo/__init__.py
@@ -47,7 +47,7 @@ from galileo_core.schemas.protect.response import Response
 from galileo_core.schemas.protect.ruleset import Ruleset
 from galileo_core.schemas.protect.stage import StageType
 
-__version__ = "1.47.0"
+__version__ = "1.48.0"
 
 __all__ = [
     "AgentSpan",
