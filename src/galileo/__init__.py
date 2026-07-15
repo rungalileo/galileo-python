3aa7fee3577a381c8e2ecd03624b7067c81682a1
galileo-automation <ci@rungalileo.io>
2026-07-15 19:53:21 +0000

    chore(release): v2.5.0
    Automatically generated by python-semantic-release

--- src/galileo/__init__.py
+++ src/galileo/__init__.py
@@ -95,7 +95,7 @@ from galileo_core.schemas.protect.response import Response
 from galileo_core.schemas.protect.ruleset import Ruleset
 from galileo_core.schemas.protect.stage import StageType
 
-__version__ = "2.4.0"
+__version__ = "2.5.0"
 
 __all__ = [
     "APIError",
